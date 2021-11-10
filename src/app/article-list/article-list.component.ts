8f5acc99691f1fc95e92ec113f13f62984f0acda
黃昱升 <tomasysh@gmail.com>
2021-11-10 10:46:09 +0800

    Day4-6: [article-list] 使用資料繫結，將資料放置到範本上

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -6,9 +6,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleListComponent implements OnInit {
 
+  article: Article = {
+    coverImg: 'https://dummyimage.com/850x350/dee2e6/6c757d.jpg',
+    createDate: 'January 1, 2021',
+    title: 'Featured Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+interface Article {
+  coverImg: string;
+  createDate: string;
+  title: string;
+  description: string;
 }
