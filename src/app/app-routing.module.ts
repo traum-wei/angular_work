144a33e0adc7795d6db299ca48b86c85ce3ac2fd
黃昱升 <tomasysh@gmail.com>
2021-11-10 10:37:50 +0800

    Day4-5: 將 AticleList 元件調整為頁面元件

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { ArticleListComponent } from './article-list/article-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'article-list', pathMatch: 'full' },
+  { path: 'article-list', component: ArticleListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-article-list',
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.css']
 })
